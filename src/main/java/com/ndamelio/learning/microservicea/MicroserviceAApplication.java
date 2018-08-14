3094bf3b2ae50d42d50be76bbb304c590a9fc170
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-14 12:17:10 -0300

    first commit

---
+++ src/main/java/com/ndamelio/learning/microservicea/MicroserviceAApplication.java
@@ -0,0 +1,12 @@
+package com.ndamelio.learning.microservicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAApplication.class, args);
+    }
+}
